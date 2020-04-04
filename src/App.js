fed725764aa0b79a9fefadac1233f83c4cf17f4f
Benjamin Ho <benjaminho@Benjamins-MacBook-Pro.local>
2020-04-03 20:53:41 -0700

    Initial front-end code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      'showRecipe': false
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.showRecipe ? 
+          <Recipe /> :
+          <form>
+            <label>Enter a calorie amount to which the meals will add up to for the day:</label>
+            <br></br>
+            <input type="text"></input>
+          </form>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Recipe extends React.Component {
+    render() {
+        return(
+            <div>
+                Hi there Ben!
+            </div>
+        );
+    }
+}
+
+export default Recipe;
