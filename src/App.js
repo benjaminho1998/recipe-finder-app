d6747706d6fc08d3504ed9d2a7181e79d4a03aee
Benjamin Ho <benjaminho@Benjamins-MacBook-Pro.local>
2020-04-04 00:32:19 -0700

    Implemented first api call

--- src/App.js
+++ src/App.js
@@ -7,19 +7,40 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      'showRecipe': false
+      'showRecipe': false,
+      'numCalories': '',
+      'showWarning': false
     };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleInput() { 
+    this.setState({'showRecipe': true});
+  };
+
+  handleChange(e) {
+    const re = /^[0-9\b]+$/;
+      if (e.target.value === '' || re.test(e.target.value)) {
+        this.setState({'numCalories': e.target.value});
+        this.setState({'showWarning': false});
+      } else {
+        this.setState({'showWarning': true});
+      };
   }
 
   render() {
     return(
       <div>
         {this.state.showRecipe ? 
-          <Recipe /> :
-          <form>
+          <Recipe numCalories={this.state.numCalories}/> :
+          <form onSubmit={this.handleInput}>
             <label>Enter a calorie amount to which the meals will add up to for the day:</label>
             <br></br>
-            <input type="text"></input>
+            <input type="text" onChange={this.handleChange} value={this.state.numCalories}></input>
+            <button type="button" onClick={this.handleInput}>Go!</button>
+            {this.state.showWarning ? (<div>Please only enter numbers.</div>) : null}
           </form>
         }
       </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 
 class Recipe extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            img: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate", {
+            "credentials": 'include',
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "x-rapidapi-key": "7c5d80fdc1msh37c56aa01298fe6p135c56jsn79df1910ca9f"
+            },
+            "params": {
+                "targetCalories": "2000",
+                "timeFrame": "day"
+            }
+        }).then((response)=>{
+            console.log(response)
+            this.setState({img: response.data.meals[0].image})
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+
     render() {
+        const url = this.state.img
         return(
             <div>
-                Hi there Ben!
+                <img alt="" src={`https://spoonacular.com/recipeImages/${url}`}></img>
             </div>
         );
     }
